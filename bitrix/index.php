490889496d339297aa38a263437502ff1c574eb9
ksn38 <ksn38@yandex.ru>
2023-05-19 21:43:45 +0300

    -

--- bitrix/index.php
+++
@@ -1,1 +0,0 @@
-<?define("WIZARD_DEFAULT_SITE_ID", "s1");require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/classes/general/wizard.php");$wizard = new CWizard("bitrix:demo");$wizard->Install();require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/epilog_after.php");?>
